a34db67dc5f650736893e2883df46d5d89cf9d9f
minna <32199029+mshroom@users.noreply.github.com>
2018-11-15 20:00:24 +0200

    added a simple text ui, Item interface and Book class

---
+++ src/main/java/domain/Book.java
@@ -0,0 +1,68 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Book implements Item{
+    private String title;
+    private String author;
+    private String url;
+    private String isbn;
+    private List<String> tags;
+    private String description;
+    
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+        List<String> tags = new ArrayList<>();
+    }    
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }    
+    
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<String> getTags() {
+        return tags;
+    }
+    
+    public void addTag(String tag) {
+        this.tags.add(tag);
+    }
+    
+}
---
+++ src/main/java/domain/Item.java
@@ -0,0 +1,17 @@
+package domain;
+
+import java.util.List;
+
+public interface Item {
+
+    String getTitle();
+    void setTitle(String title);
+    String getAuthor();
+    void setAuthor(String author);
+    String getDescription();
+    void setDescription(String description);
+    String getUrl();
+    void setUrl(String url);
+    List<String> getTags();
+    void addTag(String tag);
+}
