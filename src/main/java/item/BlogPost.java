8dd3a692d79b9741f5e6be48b46f9a9e3093b4be
Lauri Väänänen <lauri.e.vaananen@helsinki.fi>
2018-11-20 23:10:41 +0200

    Added different items to inherit from base class

--- src/main/java/item/BlogPost.java
+++ src/main/java/item/BlogPost.java
@@ -3,73 +3,9 @@ package item;
 import java.util.ArrayList;
 import java.util.List;
 
-public class BlogPost implements Item {
-	private String title;
-	private String url;
-	private String description;
-	private String author;
-	private List<String> tags;
-	
+public class BlogPost extends Base {
 
 	public BlogPost(String title, String url) {
-		this.title = title;
-		this.url = url;
-		this.tags = new ArrayList<>();
+		super(title, url);
 	}
-
-	@Override
-	public String getTitle() {
-		return this.title;
-	}
-
-	@Override
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	@Override
-	public String getAuthor() {
-		return this.author;
-	}
-
-	@Override
-	public void setAuthor(String author) {
-		this.author = author;
-	}
-
-	@Override
-	public String getDescription() {
-		return this.description;
-	}
-
-	@Override
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	@Override
-	public String getUrl() {
-		return this.url;
-	}
-
-	@Override
-	public void setUrl(String url) {
-		this.url = url;
-	}
-
-	@Override
-	public List<String> getTags() {
-		return this.tags;
-	}
-
-	@Override
-	public void addTag(String tag) {
-		this.tags.add(tag);
-	}
-	
-	@Override
-	public String toString() {
-		return "Blog post: " + this.title + " by " + this.author;
-	}
-	
 }
--- src/main/java/item/Book.java
+++ src/main/java/item/Book.java
@@ -3,71 +3,25 @@ package item;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Book implements Item{
-    private String title;
-    private String author;
-    private String url;
+public class Book extends Base{
     private String isbn;
-    private List<String> tags;
-    private String description;
     
-    public Book(String title, String author) {
-        this.title = title;
-        this.author = author;
-        this.tags = new ArrayList<>();
-    }    
-
-    public String getTitle() {
-        return title;
+    public Book(String title, String url) {
+        super(title, url);
+        this.setAuthor(url);
     }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
+    
     public String getIsbn() {
         return isbn;
     }
 
     public void setIsbn(String isbn) {
         this.isbn = isbn;
-    }    
-    
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public List<String> getTags() {
-        return tags;
-    }
-    
-    public void addTag(String tag) {
-        this.tags.add(tag);
     }
 
     @Override
     public String toString() {
-        return "Book: " + this.title + " by " + this.author;
+        return "Book: " + this.getTitle() + " by " + this.getAuthor();
     }
     
 }
